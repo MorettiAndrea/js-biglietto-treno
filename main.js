258cd41b1c7aa57796c6bc9dd519b576115034ca
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-01 17:02:42 +0200

    fine minor discount

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 // #### DATI ####
 
-const userAge = prompt(`Quanti anni hai?`);
-const userHowMuch = prompt(`quanti chilometri poercorri?`);
-let costPerKm = parseInt(`€0,21`);
-const traveled = parseInt(`100km`);
+const userAge = parseInt(prompt(`Quanti anni hai?`));
+const distance = prompt(`quanti chilometri percorri?`);
+let costPerKm = 0.21;
+// let discount = 1;
 
+let totalCost = costPerKm * distance;
 // log
 
 console.log("userAge:", userAge);
-console.log("userHowMuch:", userHowMuch);
-console.log("costPerKm:", 0.21);
-console.log("traveled:", 100);
+console.log("distance:", distance);
+console.log("costPerKm:", costPerKm);
+// console.log("discount:", discount);
+
+// risoluzione
+
+const minorDiscount = userAge >= 0 && userAge <= 18;
+
+if (minorDiscount == true) {
+  console.log("minorDiscount", minorDiscount);
+  //   discount = 0.8;
+  totalCost = totalCost * 0.8;
+}
+
+console.log("totalCost:", totalCost);
