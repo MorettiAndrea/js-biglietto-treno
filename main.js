a1072dd599c6586a19aeca68b1a5cadc2e68ad8c
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-01 17:12:37 +0200

    fine piazzamento elder discount provo bonus

--- main.js
+++ main.js
@@ -3,24 +3,31 @@
 const userAge = parseInt(prompt(`Quanti anni hai?`));
 const distance = prompt(`quanti chilometri percorri?`);
 let costPerKm = 0.21;
-// let discount = 1;
-
 let totalCost = costPerKm * distance;
 // log
 
 console.log("userAge:", userAge);
 console.log("distance:", distance);
 console.log("costPerKm:", costPerKm);
-// console.log("discount:", discount);
 
 // risoluzione
+
+// minor discount
 
 const minorDiscount = userAge >= 0 && userAge <= 18;
 
 if (minorDiscount == true) {
   console.log("minorDiscount", minorDiscount);
-  //   discount = 0.8;
   totalCost = totalCost * 0.8;
+}
+
+// elder discount
+
+const elderDiscount = userAge >= 65;
+
+if (elderDiscount == true) {
+  console.log("elderDiscount", elderDiscount);
+  totalCost = totalCost * 0.6;
 }
 
 console.log("totalCost:", totalCost);
