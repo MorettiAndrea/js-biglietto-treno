e7a4e6643cb5d065bfc85fd4767fcf5a93345a95
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-01 16:03:03 +0200

    inizio svolgimento

---
+++ main.js
@@ -0,0 +1,13 @@
+// #### DATI ####
+
+const userAge = prompt(`Quanti anni hai?`);
+const userHowMuch = prompt(`quanti chilometri poercorri?`);
+let costPerKm = parseInt(`€0,21`);
+const traveled = parseInt(`100km`);
+
+// log
+
+console.log("userAge:", userAge);
+console.log("userHowMuch:", userHowMuch);
+console.log("costPerKm:", 0.21);
+console.log("traveled:", 100);
